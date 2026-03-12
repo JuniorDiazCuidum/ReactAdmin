12ee50c513ce9261ac60cb9fd9a90d5ee3b45289
Junior <junior.diaz@cuidum.com>
2026-03-12 10:54:27 +0100

    Update

--- src/components/freatured/Featured.jsx
+++ src/components/freatured/Featured.jsx
@@ -3,7 +3,7 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpOutlinedIcon from "@mui/icons-material";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
 
 
 const Featured = () => {
@@ -23,8 +23,22 @@ const Featured = () => {
                 <div className="summary">
                     <div className="item">
                         <div className="itemTitle">Target</div>
-                        <div className="itemResult">
-                            <KeyboardArrowDownIcon />
+                        <div className="itemResult negative">
+                            <KeyboardArrowDownIcon fontSize="small" />
+                            <div className="resultAmount">$12.4k</div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="itemTitle">Last Week</div>
+                        <div className="itemResult positive">
+                            <KeyboardArrowUpOutlinedIcon fontSize="small" />
+                            <div className="resultAmount">$12.4k</div>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="itemTitle">Last Month</div>
+                        <div className="itemResult positive">
+                            <KeyboardArrowUpOutlinedIcon fontSize="small" />
                             <div className="resultAmount">$12.4k</div>
                         </div>
                     </div>
@@ -35,3 +49,4 @@ const Featured = () => {
 };
 
 export default Featured;
+
